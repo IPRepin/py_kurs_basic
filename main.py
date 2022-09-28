774d49c38bf8188f60ad8ee17b19bfe2304d0319
Pavel Repin <sghcd2012@yandex.ru>
2022-09-28 15:09:48 +0300

    add "def _get_upload_link"

--- main.py
+++ main.py
@@ -23,13 +23,26 @@ class vk_foto_in_yd():
             'photo_sizes': '1',
         }
         req = requests.get(f'{photos_get_url}', params={**self.params_vk, **photos_get_params}).json()
-        return req
+        return req['response']['items']
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.yd_id)}
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        url_file_path = None # доделать логику получения ссылки на файл
+        params = {"path": url_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
 
 
 
 def main():
-    # vk_id = input('Введите id пользователя: ')
-    vk_client = vk_foto_in_yd(304437267, yd_token)
+    vk_id = input('Введите id пользователя: ')
+    vk_client = vk_foto_in_yd(vk_id, yd_token)
     pprint(vk_client.photos_get())
 
 if __name__ == '__main__':
